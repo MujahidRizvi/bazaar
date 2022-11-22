c6e3cf3c7e8ea33b19a48b241111beb896363a58
MujahidRizvi <superizvi@gmail.com>
2022-11-22 17:02:14 +0500

    com

--- cypress/fixtures/wrldData.cy.ts
+++ cypress/fixtures/wrldData.cy.ts
@@ -1,5 +1,5 @@
 export let homeData = {
-  wrldUrlStg: "https://www.wrld.xyz",
+  bazUrlStg: "https://bazaar-dev.weom.xyz/",
   userInfoUrl: "https://staging-gateway.wrld.xyz/users/GetUserbySessionToken/?csrf-bypass=true",
   updateScreenNameUrl: "https://staging-gateway.wrld.xyz/users/UpdateUserScreenName/1?csrf-bypass=true",
   btoken: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0",
